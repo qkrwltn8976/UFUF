cea19990b8a3f2382b62cfda7883fb174c8903b9
qkrwltn8976 <qkrwltn8976@naver.com>
2019-10-01 14:52:43 +0900

    Initial commit

---
+++ view controllers/Main/OrderCompleteVC.swift
@@ -0,0 +1,30 @@
+//
+//  OrderCompleteVC.swift
+//  UFUF
+//
+//  Created by 박지수 on 01/10/2019.
+//  Copyright © 2019 박지수. All rights reserved.
+//
+
+import UIKit
+
+class OrderCompleteVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
